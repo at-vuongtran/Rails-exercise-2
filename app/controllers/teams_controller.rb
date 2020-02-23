12e14bc6bfdd10efdfea7d0e600e49478db79501
at-vuongtran <vuong.tran@asiantech.vn>
2020-02-23 08:59:17 +0700

    save a tmp

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,13 +1,32 @@
 class TeamsController < ApplicationController
-  def index; end
+  def index
+    @teams = Team.all
+  end
 
-  def show; end
+  def show
+    @team = Team.includes(:users).find(params[:id])
+  end
 
-  def new; end
+  def new
+    @team = Team.new
+  end
 
-  def create; end
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to team_path(@team)
+    else
+      render 'new'
+    end
+  end
 
-  def edit; end
+  def team_params
+    params.require(:team).permit(:name, :leader_id)
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+  end
 
   def update; end
 
